9d0dab7848871d72ae25b9fb2af048c292e00a04
alexhkurz <alexhkurz@gmail.com>
2020-05-05 13:47:24 -0700

    revertedtooldwat2wasm

--- Compiler/test/wat2wasm.js
+++ Compiler/test/wat2wasm.js
@@ -6,13 +6,9 @@ const inputWat = process.argv[2];
 const outputWasm = inputWat.replace(/\.[^.]+$/, '.wasm');
 
 
-try{
-  const wasmModule = wabt.parseWat(inputWat, readFileSync(inputWat, "utf8"));
-  wasmModule.resolveNames();
-  wasmModule.validate();
-  const { buffer } = wasmModule.toBinary({});
+const wasmModule = wabt.parseWat(inputWat, readFileSync(inputWat, "utf8"));
+wasmModule.resolveNames();
+wasmModule.validate();
+const { buffer } = wasmModule.toBinary({});
 
-  writeFileSync(outputWasm, Buffer.from(buffer));
-} catch (error) {
-  console.error(error);
-}
+writeFileSync(outputWasm, Buffer.from(buffer));
